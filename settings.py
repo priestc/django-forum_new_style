2ef92826f5822c6606eff65a63947ac4d119001c
Your Name <cp368202@ohiou.edu>
2010-07-01 01:47:24 -0400

    added settings file, also some documentation

---
+++ settings.py
@@ -0,0 +1,9 @@
+# the name of the fourm, used in the RSS feeds
+FORUM_OVERALL_NAME = "Default Forum"
+
+#whether or not to use recaptcha for unregistered users
+FORUM_USE_RECAPTCHA = False
+
+# key for recaptcha to work. Get from the recaptcha project's page
+RECAPTCHA_PRIVATE_KEY = ''
+RECAPTCHA_PUBLIC_KEY = ''
